7cae8e7787ef6cb24b52085c3c20958a4a7928ba
KaiKai <jecalaycay17@gmail.com>
2022-10-21 14:58:18 +0800

    project start, navbar and mobile view

---
+++ tailwind.config.cjs
@@ -0,0 +1,17 @@
+const config = {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+
+	theme: {
+		extend: {}
+	},
+	// variants: {
+	// 	extend: {
+	// 		display: ['group-focus'],
+	// 		opacity: ['group-focus'],
+	// 		inset: ['group-focus']
+	// 	},
+	// },
+	plugins: []
+};
+
+module.exports = config;
